45ed4004263392154644305766da143d74a16d7a
Yauheni Palupanau <yevg.polupanov@gmail.com>
2025-10-21 16:43:29 +0200

    Initial Commit

---
+++ Miracast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Miracast
+//
+//  Created by Yauheni Palupanau on 21/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Miracast/MiracastApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiracastApp.swift
+//  Miracast
+//
+//  Created by Yauheni Palupanau on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MiracastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
